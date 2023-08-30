7ecf445a168cfd9d428aa4b06d8ae7d36e24e1c2
mccoycoding <mccoycoding@gmail.com>
2023-08-30 01:36:36 -0700

    emptied app.jsx and added basic fetch requests

---
+++ src/helpers/fetchRequests.js
@@ -0,0 +1,24 @@
+const URL = "https://fakestoreapi.com"
+
+//GET Products
+export async function fetchAllProducts(page) {
+    const limit = page * 10
+    try {
+        const response = await fetch(`${URL}/products?limit=${limit}`);
+        const result = response.json();
+        return result;
+    } catch (error) {
+        console.error(`ERROR FETCHING ALL PRODUCTS: ${error}`);
+    }
+};
+
+//GET Single Product
+export async function fetchSingleProduct(id) {
+    try {
+        const response = await fetch(`${URL}/products/${id}`);
+        const result = response.json();
+        return result;
+    } catch (error) {
+        console.error(`ERROR FETCHING ITEM#${id}: ${error}`);
+    }
+};
